51c099dd0d668cd55a05b90d548b67719a6df740
Johanna <svallingson@gmail.com>
2020-05-13 21:14:05 +0200

    m

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ express()
     .get('/db', async (req, res) => {
         try {
             const client = await pool.connect()
-            const result = await client.query('SELECT * FROM teacher WHERE rumskod = hajen);
+            const result = await client.query('SELECT spelkod FROM teacher WHERE rumskod is 'hajen');
             //const results = { 'results': (result) ? result.rows : null};
             res.send(result);
             client.release();
