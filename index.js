612d1b385bba7d33d3795b4cd25f635bda57b09f
Johanna <svallingson@gmail.com>
2020-05-13 21:43:08 +0200

    params

--- index.js
+++ index.js
@@ -37,6 +37,27 @@ express()
             res.send("Error " + err);
         }
     })
+
+"someParam1=foo&someParam1=bar&someParam1=bas"
+
+
+
+    .post('/creategame/:room=?&gamecode=?', async (req, res) => {
+            try {
+                const client = await pool.connect()
+                var room = req.params.room;
+                var gamecode = req.params.gamecode;
+                const result = await client.query("INSERT INTO teacher values ('" + room + "','" + gamecode + "')" );
+                const results = { 'results': (result) ? result.rows : null};
+                res.render('pages/winner', results );
+                client.release();
+            } catch (err) {
+                console.error(err);
+                res.send("Error " + err);
+            }
+        }
+
+    )
     .listen(PORT, () => console.log(`Listening on ${ PORT }`)) //<---denna raden ska ligga sist
 
 
