819dfa1ad8be4b607724da96f3f95b269afc2290
Johanna <svallingson@gmail.com>
2020-05-14 08:41:01 +0200

    post method

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 const { Pool } = require('pg');
+var bodyParser = require('body-parser')
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL || 'postgresql://jteascrgevmsmm:a7ed6c2ec0f336ae9124c0cbb5452bd68c1f7c3a828b19bfcee2d6cad821eebe@localhost:5000/d307bgdakulge0',
     ssl: !!process.env.DATABASE_URL
 });
+pool.on('error', (err) => {
+    console.error('An idle client has experienced an error', err.stack)
+})
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
+    .use(bodyParser.json())
     .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
@@ -45,7 +50,7 @@ express()
             try {
                 const client = await pool.connect()
                 var room = req.body.room;
-                console.log(req)
+                console.log(room)
                 var gamecode = req.body.gamecode;
                 const result = await client.query("INSERT INTO teacher values ('" + room + "','" + gamecode +  "')" );
                 const results = { 'results': (result) ? result.rows : null};
@@ -53,12 +58,13 @@ express()
                 client.release();
             } catch (err) {
                 console.error(err);
-                res.send("Error " + err);
+                console.log(req.body.room + ' = req.body.room');
+                res.send("Error in post method: " + err);
             }
         }
 
     )
-    .listen(PORT, () => console.log(`Listening on ${ PORT }`)) //<---denna raden ska ligga sist
+    .listen(PORT, () => console.log(`!!Listening on ${ PORT }`)) //<---denna raden ska ligga sist
 
 
 
