e10d3f08e9f791ccf9b3c1ca23aa464aa5addf96
sebas <sebastianwaltilla@hotmail.com>
2020-05-27 14:50:21 +0200

    test 2

--- index.js
+++ index.js
@@ -141,7 +141,7 @@ express()
         try {
             const client = await pool.connect()
             const result = await client.query("SELECT counter FROM ping where id=1" );
-            const increase = result + 1;
+            const increase = result.counter + 1;
             const result2 = await client.query("UPDATE ping SET counter = " + increase + " WHERE id=1");
             const results = { 'counter': (increase) ? result.rows : null};
             res.send(results);
