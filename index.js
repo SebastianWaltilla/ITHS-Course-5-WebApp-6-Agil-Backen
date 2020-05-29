59a02ef521615363eb1bb0c2ab6b575c707c2ecc
sebas <sebastianwaltilla@hotmail.com>
2020-05-29 10:51:59 +0200

    hej

--- index.js
+++ index.js
@@ -129,7 +129,7 @@ express()
             const client = await pool.connect()
             var room = req.body.room;
             const result = await client.query("SELECT correctanswers FROM playertable WHERE room = '" + room + "' ORDER BY correctanswers ASC LIMIT 1" );
-            res.send(result.rows[0].correctanswers);
+            res.send(result.rows[0]);
             client.release();
         } catch (err) {
             console.error(err);
